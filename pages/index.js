a8c143358e41cdd0ebb876233f7c871e5c364256
soni shah <88655561+soni-ujustbe@users.noreply.github.com>
2022-09-16 10:01:34 +0530

    my first commit

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+// import { useTable } from 'react-table'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Page({ datalist }) {
+  // console.log("all data", datalist);
+
+
+  return (
+    <div>
+
+      <h1>All Albums</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Sr</th>
+            <th>meetingId</th>
+            <th>From User</th>
+            <th>To User</th>
+            <th>Meeting Agenda</th>
+            <th>Meeting Code</th>
+            <th>Meeting Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datalist.map((album, idx) => (
+            // console.log(album)
+            <tr key={idx}>
+              <td>
+                {idx + 1}
+              </td>
+              <td>
+                {album.meetingId}
+              </td>
+              <td>
+                {album.fromUserName}
+              </td>
+              <td>
+                {album.toUsersList && album.toUsersList.map((data) => <ul><li>To User : {data.toUserName}</li><li>Status : {data.status}</li></ul>)}
+              </td>
+              <td>
+              {album.agenda }
+              </td>
+              <td>{album.meetingCode}</td>
+              <td>
+                {album.meetingDate}
+              </td>
+            </tr>
+          ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://api.ujustbe.com/Meeting/details`)
+  const data = await res.json()
+  const datalist = data.data.meetingList
+
+  console.log("tottal data", datalist);
+
+  // Pass data to the page via props
+  return { props: { datalist } }
+}
+
+export default Page
